4f41414229caa8699e4153efc71fdc46b51e700d
Sumit Shrestha <summitshrestha8@gmail.com>
2024-03-08 19:07:43 -0600

    Added Admin and Completed Client Side

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -7,6 +7,7 @@ import {
   getAllHotels,
   getHotel,
   updateHotel,
+  getHotelRooms,
 } from '../controllers/hotel.js';
 
 import { verifyAdmin } from '../utils/verifyToken.js';
@@ -20,4 +21,5 @@ router.get('/find/:id', getHotel);
 router.get('/', getAllHotels);
 router.get('/countByCity', countByCity);
 router.get('/countByType', countByType);
+router.get('/room/:id', getHotelRooms);
 export default router;
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -6,11 +6,13 @@ import {
   deleteRoom,
   getRoom,
   getAllRooms,
+  updateRoomAvailability,
 } from '../controllers/room.js';
 
 const router = express.Router();
 
 router.post('/:hotelId', verifyAdmin, createRoom);
+router.put('/availability/:id', updateRoomAvailability);
 router.put('/:id', verifyAdmin, updateRoom);
 router.delete('/:id/:hotelId', verifyAdmin, deleteRoom);
 router.get('/:id', getRoom);
